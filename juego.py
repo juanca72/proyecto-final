e0d43300874dcc984341d129fe137c63981ff6ce
juanca72 <37439860+juanca72@users.noreply.github.com>
2018-05-25 21:24:24 +0200

    nuevo personaje

--- juego.py
+++ juego.py
@@ -5,15 +5,28 @@ import pygame
 class Personaje(pygame.sprite.Sprite):
     def __init__(self):
 
+        super().__init__()
+
+        alto=50
+        ancho=30
+
+        self.image=pygame.surface((alto,ancho))
+        self.image.fill((255,0,0))
+        self.rect=self.image.get_rect()
+        self.velocidad=[0,0]
+
     def update(self):
 
     def saltar(self):
 
     def mov_izquierda(self):
+        self.velocidad= -2
 
     def mov_derecha(self):
+        self.velocidad= 2
 
     def stop(self):
+        self.velocidad = 0
 
 class Plataforma(pygame.sprite.Sprite):
     def __init__(self):
