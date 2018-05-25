e66ef8f50101f22057f2043f8ca7646bfa4b18bf
Diego <diegogomezledo@gmail.com>
2018-05-25 20:34:15 +0200

    Clases definidas
    Definimos las clases personaje y plataforma

---
+++ juego.py
@@ -0,0 +1,27 @@
+import pygame
+
+#------Clases-------
+
+class Personaje(pygame.sprite.Sprite):
+    def __init__(self):
+
+    def update(self):
+
+    def saltar(self):
+
+    def mov_izquierda(self):
+
+    def mov_derecha(self):
+
+    def stop(self):
+
+class Plataforma(pygame.sprite.Sprite):
+    def __init__(self):
+
+#-----Funciones-------
+
+def main():
+    pygame.init()
+
+if __name__ == '__main__':
+    main()
